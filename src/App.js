206478b7681b64c7163574f2395d5062a97522c8
Atrem Petrenko <64548375+Artem-ka-create@users.noreply.github.com>
2023-08-28 23:20:20 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+// import { http } from 'bigbluebutton-js';
 
 import './App.css';
 
 function App() {
+
+
+
   return (
     <div className="App">
       <h1>IT IS DIPLOMA APP</h1>
+      <h1>First Commit</h1>
+
     </div>
   );
 }
