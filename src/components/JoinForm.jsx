093b329bbbf02325bb1b9b84ac26d02f09c3a68e
Atrem Petrenko <64548375+Artem-ka-create@users.noreply.github.com>
2023-08-29 16:16:45 +0200

    location test

--- src/components/JoinForm.jsx
+++ src/components/JoinForm.jsx
@@ -1,15 +1,19 @@
 import React, {useState} from 'react'
+import { useLocation } from 'react-router-dom'
 
 import SubmitButton from './UI/Button/SubmitButton'
 import Input from './UI/Input/Input'
 import styles from './JoinForm.module.css'
 
 function JoinForm() {
+    const location = useLocation();
 
     const [data,setData] = useState({url:''});
 
     const onSubmitHandler =(event)=>{
         event.preventDefault();
+        // location.push({url:'de'})
+        console.log(location);
         // createUrl(data.url);
         setData({url:''});
     };
