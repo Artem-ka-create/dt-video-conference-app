76d2ca73791001a3e9584211b298701d6626ecde
Atrem Petrenko <64548375+Artem-ka-create@users.noreply.github.com>
2024-02-02 12:38:19 +0100

    bbb-config file

--- src/data/BBBconfig.js
+++ src/data/BBBconfig.js
@@ -1,6 +1,11 @@
 const serverConfig = {
-    url: 'https://artempetrenko.site/bigbluebutton/',
-    secret: 'y0ae9d3KJRSZXvqX8BGWMS1pjabXrXpvdLr6uafQk'
+    url: 'https://bbb-dt.de/bigbluebutton/',
+    secret: 'FhFeiJ6RV25ee3D9HmrojJMrjAmyAyaoR08fZdZpM'
   }
+
+// const serverConfig = {
+//     url: 'https://artempetrenko.site/bigbluebutton/',
+//     secret: 'y0ae9d3KJRSZXvqX8BGWMS1pjabXrXpvdLr6uafQk'
+// }
 
   export default serverConfig;
