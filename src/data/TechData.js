7bd72afd201ee78caebe9786c7bbdbb702931b91
Artem Petrenko <64548375+Artem-ka-create@users.noreply.github.com>
2024-02-09 10:42:05 +0100

    Update TechData.js

--- src/data/TechData.js
+++ src/data/TechData.js
@@ -9,9 +9,9 @@ export const RoomCategories = {
 }
 
 // spring-boot-server-domain -> http://localhost:8080
-export const BASE_URL = 'https://06f2-147-232-176-71.ngrok-free.app';
+export const BASE_URL = 'https://0421-2001-9e8-98e0-b800-d13f-95cd-ba30-f734.ngrok-free.app';
 // react-client-server-domain -> http://localhost:3000
-export const CLIENT_BASE_URL = 'https://4cb0-147-232-176-71.ngrok-free.app';
+export const CLIENT_BASE_URL = 'https://3370-2001-9e8-98e0-b800-d13f-95cd-ba30-f734.ngrok-free.app';
 
 
 //encrypted secret word = "telekom"
