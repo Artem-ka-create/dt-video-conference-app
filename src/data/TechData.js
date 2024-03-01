92219db92624fbc83a33e8e1a9c5ac21aca120da
Artem Petrenko <64548375+Artem-ka-create@users.noreply.github.com>
2024-03-01 14:12:35 +0100

    add bbb logic

--- src/data/TechData.js
+++ src/data/TechData.js
@@ -11,7 +11,7 @@ export const RoomCategories = {
 // spring-boot-server-domain -> http://localhost:8080
 export const BASE_URL = 'https://b42b-147-232-176-71.ngrok-free.app';
 // react-client-server-domain -> http://localhost:3000
-export const CLIENT_BASE_URL = 'https://f749-147-232-176-71.ngrok-free.app
+export const CLIENT_BASE_URL = 'https://f749-147-232-176-71.ngrok-free.app';
 
 
 //encrypted secret word = "telekom"
