ca12632e23e17f9258081707cdbc760522b415a4
Artem Petrenko <64548375+Artem-ka-create@users.noreply.github.com>
2024-04-21 20:08:17 +0200

    about text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,10 +2,18 @@ import React from 'react'
 
 function About() {
   return (
-    <>
-    <h1>Some words about this project</h1>
-    <div>About</div>
-    </>
+      <>
+        <h1>DTMeet video conferencing platform</h1>
+        <div>This application was developed as a practical part of diploma thesis</div>
+        <h3><b>Integration of open-source communication platforms into Telekom interoperability client</b></h3>
+        <h4>Technical University of Košice 2024</h4>
+
+        <div>The deal of my thesis was to create platform for conducting video conferences</div>
+        <div>by integrating Jitsi Meet and BigBlueButton video conferencing systems.</div>
+
+        <h3>More detailed info about project can be found in my git repository</h3>
+        <div><b>https://github.com/Artem-ka-create/dt-video-conference-app</b></div>
+      </>
   )
 }
 
