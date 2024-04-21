1d482f0c29d56d11b10c9923bb727bb4f18c1d7b
Artem Petrenko <64548375+Artem-ka-create@users.noreply.github.com>
2024-04-21 19:54:30 +0200

    search room , loop

--- src/components/RoomComponents/RoomListComponent.jsx
+++ src/components/RoomComponents/RoomListComponent.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useReducer} from 'react'
 import AddRoom from './AddRoom';
 import Room from './Room';
 import styles from './RoomList.module.css';
@@ -6,14 +6,49 @@ import {useNavigate, useLocation} from "react-router-dom";
 
 
 import useAxiosPrivate from '../../hooks/useAxiosPrivate';
+import {ProgressSpinner} from "primereact/progressspinner";
 
 function RoomListComponent({showToastEvent}) {
 
     const [search, setSearch] = useState('');
     const [rooms, setRooms] = useState([]);
+    const [roomSave, setRoomSave] = useState([]);
     const axiosPrivate = useAxiosPrivate();
     const navigate = useNavigate();
     const location = useLocation();
+    const [load,setLoad] = useState(false);
+
+
+    useEffect(() => {
+
+
+    }, [search])
+
+    const searchHandler = (text) => {
+        setLoad(true)
+        console.log("SEARCH=> ", text)
+
+        if (text === '') {
+            setRooms(roomSave)
+        } else {
+            // const filterBySearch =
+            let arr = roomSave.filter((room) => room.name.toLowerCase().includes(text.toLowerCase()))
+            console.log("flter=>", arr)
+            setRooms(arr)
+        }
+        setSearch(text)
+    }
+
+
+    useEffect(() => {
+        console.log("roms-", rooms)
+            // forceUpdate()
+        setTimeout(()=> {
+            setLoad(false)
+        }, 500)
+
+    }, [rooms]);
+
 
     useEffect(() => {
         let isMounted = true;
@@ -26,7 +61,10 @@ function RoomListComponent({showToastEvent}) {
                     signal: controller.signal
                 });
                 console.log('RESPONSEEE-> ', response.data);
-                isMounted && setRooms(response.data);
+                if (isMounted) {
+                    setRooms(response.data)
+                    setRoomSave(response.data)
+                }
             } catch (err) {
                 console.error(err);
                 navigate('/signin', {state: {from: location}, replace: true});
@@ -47,14 +85,20 @@ function RoomListComponent({showToastEvent}) {
             <h2>Your Rooms</h2>
             <div className={styles.roomSearchContainer}>
                 <input placeholder='Find your room...' id='RoomSearch' value={search}
-                       onChange={(event) => setSearch(event.target.value)}/>
+                       onChange={(event) => searchHandler(event.target.value)}/>
             </div>
             {/* <button onClick={useRefreshToken()}>REFREDSH</button> */}
             <div className={styles.roomListContainer}>
 
-                {rooms.map((item, i) => <Room key={i} roomDetail={item} showToast={showToastEvent} roomsArr={rooms}
-                                              roomInitialize={setRooms} />)}
-                <AddRoom showToast={showToastEvent} roomsArr={rooms} roomInitialize={setRooms}/>
+                {load ?  <ProgressSpinner style={{width: '4.4rem', height: '4.4rem'}} strokeWidth="5" animationDuration=".8s"/> :
+                <>
+                    {rooms.map((item, i) =><Room key={i} roomDetail={item} showToast={showToastEvent} roomsArr={rooms}
+                                                 roomInitialize={setRooms}/>)}
+                    <AddRoom showToast={showToastEvent} roomsArr={rooms} roomInitialize={setRooms}/>
+                </>
+                }
+
+
             </div>
 
 
