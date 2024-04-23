2f232f7d34e21d083535402e512c5ce234fd243c
Artem Petrenko <64548375+Artem-ka-create@users.noreply.github.com>
2024-04-23 14:33:13 +0200

    about content upd

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,16 +3,17 @@ import React from 'react'
 function About() {
   return (
       <>
-        <h1>DTMeet video conferencing platform</h1>
-        <div>This application was developed as a practical part of diploma thesis</div>
-        <h3><b>Integration of open-source communication platforms into Telekom interoperability client</b></h3>
-        <h4>Technical University of Košice 2024</h4>
+          <h1>DTMeet video conferencing platform</h1>
+          <div>This application was developed as a practical part of diploma thesis</div>
+          <h3><b>Integration of open-source communication platforms into Telekom interoperability client</b></h3>
+          <h4>Technical University of Košice 2024</h4>
 
-        <div>The deal of my thesis was to create platform for conducting video conferences</div>
-        <div>by integrating Jitsi Meet and BigBlueButton video conferencing systems.</div>
+          <div>The deal of my thesis was to create platform for conducting video conferences</div>
+          <div>by integrating Jitsi Meet and BigBlueButton video conferencing systems.</div>
 
-        <h3>More detailed info about project can be found in my git repository</h3>
-        <div><b>https://github.com/Artem-ka-create/dt-video-conference-app</b></div>
+          <h3>More detailed info about project can be found in my git repository</h3>
+          <div>Client-side of application - <b>https://github.com/Artem-ka-create/dt-video-conference-app</b></div>
+          <div>Server-side of application - <b>https://github.com/Artem-ka-create/video-conference-api</b></div>
       </>
   )
 }
